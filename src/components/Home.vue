f10df747e54b653bce069804d8ff96ae8efe3667
Duy <duylqh@adgad-net.vn>
2022-10-05 16:40:17 +0700

    routing-homeComponent

---
+++ src/components/Home.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>Welcome User to Home Page</h1>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name:'Home',
+}
+</script>
