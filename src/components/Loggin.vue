6b030a6553ae105f9640c35ba2be4587a15c8fc6
Duy <duylqh@adgad-net.vn>
2022-10-06 08:51:05 +0700

    login-api-integration

--- src/components/Loggin.vue
+++ src/components/Loggin.vue
@@ -2,10 +2,9 @@
   <img class="logo" src="../assets/logo.png" />
   <h1>Loggin</h1>
   <div class="login">
-    <input type="text" v-model="name" placeholder="Enter Name" />
     <input type="text" v-model="email" placeholder="Enter Email" />
     <input type="password" v-model="password" placeholder="Enter Password" />
-    <button >Loggin</button>
+    <button v-on:click="login">Loggin</button>
     <p>
       <router-link to="/sign-up">Sign Up</router-link>
     </p>
@@ -13,8 +12,33 @@
 </template>
 
 <script>
+import axios from "axios";
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
   name: "Loggin",
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    async login() {
+      let result = await axios.get(
+        `http://localhost:3000/users?email=${this.email}&password=${this.password}`
+      );
+
+      if (result.status == 200 && result.data.length > 0) {
+        localStorage.setItem("user-info", JSON.stringify(result.data[0]));
+        this.$router.push({ name: "Home" });
+      }
+    },
+  },
+  mounted() {
+    let user = localStorage.getItem("user-info");
+    if (user) {
+      this.$router.push({ name: "Home" });
+    }
+  },
 };
 </script>
