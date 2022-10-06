dac70f377c82c3f44de8bf54ddf85686d5ef05f4
Duy <duylqh@adgad-net.vn>
2022-10-06 09:05:02 +0700

    heaeder

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,16 @@
 <template>
+  <header-component />
     <h1>Welcome User to Home Page</h1>
 </template>
 
 <script>
+import HeaderComponent from './HeaderComponent.vue';
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
   name:'Home',
+  components:{
+    HeaderComponent
+  },
   mounted() {
     let user = localStorage.getItem('user-info');
     if(!user)
